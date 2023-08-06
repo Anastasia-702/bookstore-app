251c0722347d773895b2aafbe3260c74b7aa18c6
Anastasia <anastasian817@gmail.com>
2023-08-06 19:29:34 +0200

    implemented BookRepository, BookService

---
+++ src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,50 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.model.Book;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory factory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert book into DB " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from Book", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books from db", e);
+        }
+    }
+}
